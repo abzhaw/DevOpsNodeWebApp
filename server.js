39e4da8538444dfd017949f9819e0cceb822df38
abzhaw <raphael.aberer@outlook.com>
2025-05-11 21:33:41 +0200

    sdf

--- server.js
+++ server.js
@@ -3,13 +3,13 @@
 const express = require('express');
 
 // Constants
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 const HOST = '0.0.0.0';
 
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('(-: Hello FS2025 DevOps Course! :-)');
+  res.send('Hello FS2025 DevOps Course! Cloud Deploy auf Azure with GitHub Actions!');
 });
 
 app.listen(PORT, HOST);

d60208fad7d6d7a0115d36007c2a080bc6f53a7f
abzhaw <raphael.aberer@outlook.com>
2025-05-16 20:34:38 +0200

    azure

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello FS2025 DevOps Course! RAPHAEL ABERER - Cloud + LOKAL!');
+  res.send('Hello FS2025 DevOps Course! RAPHAEL ABERER - AZURE Cloud + LOKAL!');
 });
 
 app.listen(PORT, HOST);
